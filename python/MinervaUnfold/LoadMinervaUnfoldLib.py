8d6817e1aaf387ef7cbeccaf4703c7662fd8ecf5
Heidi Schellman <heidi.schellman@oregonstate.edu>
2024-12-14 12:36:56 -0800

    add in python bindings

---
+++ python/MinervaUnfold/LoadMinervaUnfoldLib.py
@@ -0,0 +1,60 @@
+"""
+  LoadMInervaUnfoldLib.py:
+   The code necessary to load the libplotutils.so library
+   so that PlotUtils C++ objects are available.
+  
+   Original author: J. Wolcott (jwolcott@fnal.gov)
+                    November 2012
+"""
+
+# hms 2021-11-20 comment out classes that moved to MAT-MINERvA
+# to use this you need to
+# export PYTHONPATH=$WHEREMATIS/MAT/python:$WHEREMATIS/MAT/python/PlotUtils
+
+import sys
+import os
+
+import ROOT
+
+# List of classes to copy from the Reflex library into the "PlotUtils" namespace
+CLASSES_TO_LOAD = [
+# from MinervaUnfold
+    "MnvResponse",
+    "MnvUnfold"
+]
+
+# new code for ROOT6
+if os.getenv("PLOTUTILSVERSION")=="ROOT6":
+# replace with voodoo found at https://gitlab.cern.ch/clemenci/Gaudi/commit/47772dfbb429a1392e12143403314e0abb45322d
+    try:
+        import PyCintex # needed to enable Cintex if it exists
+        del PyCintex
+    except ImportError:
+        pass
+else:
+    import PyCintex
+
+#import PlotUtils
+import MinervaUnfold
+
+# the ROOT interpreter gobbles up any arguments
+# that might be in sys.argv, whether you like it or not.
+# we need to tuck them away for later.
+args = sys.argv[:]
+sys.argv = sys.argv[:0]
+
+if "UNFOLDUTILSROOT" in os.environ:
+  #ROOT.gSystem.Load("libMAT")
+  ROOT.gSystem.Load("libMinervaUnfold")
+
+	# copy the classes from the Reflex library
+	# into the "PlotUtils" namespace for more
+	# straightforward access.
+  for cls in CLASSES_TO_LOAD:
+    setattr(MinervaUnfold, cls, getattr(ROOT.MinervaUnfold, cls))
+else:
+	print ( "Note: $UNFOLDUTILSROOT is not defined in the current environment.  PlotUtils libraries were not loaded.")
+	
+# now that ROOT has done its thing,
+# we can restore the arguments...
+sys.argv = args
